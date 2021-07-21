8e06df64fb36b8c469aed61e245a52811f84d77c
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2021-07-21 09:49:34 -0300

    Initial Commit

---
+++ RickAndMortyiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyiOSApp
+//
+//  Created by Luiz Eduardo do Prado on 21/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
